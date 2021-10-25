bb8a01df9179051272f3078acc9b60960c80cf2f
jiangyx <1773279493@qq.com>
2021-10-25 15:26:48 +0800

    Springboot2入门

---
+++ Study/src/main/java/com/example/study/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.example.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiangyx
+ * @version 1.0
+ * @date 2021/10/25 15:24
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Spring Boot2!";
+    }
+}
